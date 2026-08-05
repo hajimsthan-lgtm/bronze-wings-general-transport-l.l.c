b49d4de3c77f93f3dc798d81f292f3c74a26c46e
Driving license Typing services <drivemetyping@gmail.com>
2026-08-05 11:14:47 +0000

    Visual edits

--- src/components/ui/sheet.jsx
+++ src/components/ui/sheet.jsx
@@ -49,7 +49,7 @@ const SheetContent = React.forwardRef(({ side = "right", className, children, ..
     <SheetOverlay />
     <SheetPrimitive.Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
       <SheetPrimitive.Close
-      className="absolute left-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+      className="absolute left-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary hidden">
         
         <span className="sr-only">Back</span>
       </SheetPrimitive.Close>
