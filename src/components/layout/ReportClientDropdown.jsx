f375aa6b4e63276df026ce94f20d7f5f19f92714
Driving license Typing services <drivemetyping@gmail.com>
2026-08-20 16:14:55 +0000

    Visual edits

--- src/components/layout/ReportClientDropdown.jsx
+++ src/components/layout/ReportClientDropdown.jsx
@@ -13,15 +13,15 @@ export default function ReportClientDropdown() {
 
   return (
     <Select value={value} onValueChange={setReportClient}>
-      <SelectTrigger className="w-[130px] sm:w-[200px] h-8 bg-white/5 border-white/10 text-foreground text-xs">
+      <SelectTrigger className="w-[130px] sm:w-[200px] h-8 bg-white/5 border-white/10 text-foreground text-xs hidden">
         <SelectValue placeholder="All Clients" />
       </SelectTrigger>
       <SelectContent>
         <SelectItem value="all">All Clients</SelectItem>
-        {clients.map((c) => (
-          <SelectItem key={c.id} value={c.name}>{c.name}</SelectItem>
-        ))}
+        {clients.map((c) =>
+        <SelectItem key={c.id} value={c.name}>{c.name}</SelectItem>
+        )}
       </SelectContent>
-    </Select>
-  );
+    </Select>);
+
 }
