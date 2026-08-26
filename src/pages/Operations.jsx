561220cffcb245ccc1e846c8061a8261a6d607b5
Driving license Typing services <drivemetyping@gmail.com>
2026-08-26 16:56:49 +0000

    Refactor operations page to use TripsList row-card design

--- src/pages/Operations.jsx
+++ src/pages/Operations.jsx
@@ -458,7 +458,7 @@ export default function Operations() {
             )}
             {showTrips && filteredTrips.length > 0 && (
               isMobile ? (
-                <MobileAuroraTripsTable
+                <TripsList
                   trips={filteredTrips}
                   onOpenDetail={openDetailTrip}
                   onEdit={openEditTrip}
@@ -468,6 +468,7 @@ export default function Operations() {
                   vehicleMap={vehicleMap}
                   clientMap={clientMap}
                   invoiceMap={invoiceMap}
+                  onInvoicesChanged={() => refetchInvoices()}
                   onBulkStatus={handleBulkTripStatus}
                   onBulkDelete={handleBulkTripDelete}
                 />
